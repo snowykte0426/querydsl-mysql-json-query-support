9d1b3f5348c0e92226709ecfd553618caa399f7d
snowykte0426 <snowykte0426@naver.com>
2026-01-13 03:27:54 +0900

    refactor build.gradle.kts to apply plugins and configurations at the project and subproject levels

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,73 @@
 plugins {
-    `java-library`
-    `maven-publish`
+    `java-library` apply false
+    `maven-publish` apply false
 }
 
-group = "com.github.snowykte0426"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "com.github.snowykte0426"
+    version = "1.0-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
     }
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-    withSourcesJar()
-    withJavadocJar()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = "querydsl-mysql-json-query-support"
-            version = project.version.toString()
+    configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(25)
+        }
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        withSourcesJar()
+        withJavadocJar()
+    }
 
-            from(components["java"])
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
 
-            pom {
-                name = "QueryDSL MySQL JSON Query Support"
-                description = "A QueryDSL extension for MySQL JSON query support."
-                url = "https://github.com/snowykte0426/querydsl-mysql-json-query-support"
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 
-                licenses {
-                    license {
-                        name = "MIT License"
-                        url = "https://opensource.org/licenses/MIT"
-                    }
-                }
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                from(components["java"])
 
-                developers {
-                    developer {
-                        id = "snowykte0426"
-                        name = "Kim Tae Eun"
-                        email = "snowykte0426@naver.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://github.com/snowykte0426/querydsl-mysql-json-query-support.git"
-                    developerConnection = "scm:git:ssh://github.com:snowykte0426/querydsl-mysql-json-query-support.git"
+                pom {
+                    name = "QueryDSL MySQL JSON Query Support - ${project.name}"
+                    description = "A QueryDSL extension for MySQL JSON query support - ${project.name} module"
                     url = "https://github.com/snowykte0426/querydsl-mysql-json-query-support"
+
+                    licenses {
+                        license {
+                            name = "MIT License"
+                            url = "https://opensource.org/licenses/MIT"
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id = "snowykte0426"
+                            name = "Kim Tae Eun"
+                            email = "snowykte0426@naver.com"
+                        }
+                    }
+
+                    scm {
+                        connection = "scm:git:git://github.com/snowykte0426/querydsl-mysql-json-query-support.git"
+                        developerConnection = "scm:git:ssh://github.com:snowykte0426/querydsl-mysql-json-query-support.git"
+                        url = "https://github.com/snowykte0426/querydsl-mysql-json-query-support"
+                    }
                 }
             }
         }
     }
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.release.set(17)
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
